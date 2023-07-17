992c859c17a00d3cee2b7651c3bdd7d23114206f
Nixuge <mail@nixuge.me>
2023-07-17 15:12:08 +0200

    Bumped ver (1.2.2->1.2.3)

--- src/main/java/me/nixuge/epiczoomer/McMod.java
+++ src/main/java/me/nixuge/epiczoomer/McMod.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class McMod {
     public static final String MOD_ID = "epiczoomer";
     public static final String NAME = "Epic Zoomer";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
 
 
     @Getter
